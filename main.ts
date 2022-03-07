57814b37c89d819ca1b40cc42b2facb236035f95
Maria Grandl <97896791+grandlmar@users.noreply.github.com>
2022-03-07 17:59:31 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.pause(1000)
+basic.showIcon(IconNames.Happy)
+basic.showLeds(`
+    . . . . .
+    . . . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+basic.pause(200)
+basic.showIcon(IconNames.Happy)
